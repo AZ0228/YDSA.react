384a7df11585e4fb6a303f91892c3eea3513df4a
AZ0228 <jbliu88@gmail.com>
2023-10-05 23:54:48 -0400

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+
+
+const logo = process.env.PUBLIC_URL + '/LOGO.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
